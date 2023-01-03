8b152282ebcda4c68a6a56b138c78be2cfd311c1
Wen Huang <littleprincewen@gmail.com>
2023-01-03 17:16:58 +0100

    test pointer for strings

--- 1.1/test.cpp
+++ 1.1/test.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+#include <string.h>
+// #include <cstring>
+
+using namespace std;
 int main()
 {
-	return 0;
+	
+	char buff[11] = "abcdefghij";
+	char *label=buff;
+	cout << (*label) << endl;// a
+	cout << &(*label) << endl;//abcdefghij
+	cout << (void*)&(*label) << endl;//0x16f0f6de8
+	cout << label << endl;//abcdefghij
+	cout << &label << endl;//0x16f0f6dd8
+	cout << (void*)&label << endl;//0x16f0f6dd8
+
 }
